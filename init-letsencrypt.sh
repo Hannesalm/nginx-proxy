7144af61bf0620fa54f4287c7cc04b1bf274e77b
Hannes Alm <hannes@almar.se>
2019-02-08 00:13:04 +0100

    commit

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-domains=(dital.nu www.dital.nu)
+domains=(kna.dital.nu)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="hannes@almar.se" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
